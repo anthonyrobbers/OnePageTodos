df573a1e0d6ca57a7ef960d6904bee896d82403b
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-03 10:50:10 -0400

    adding in template for building the display todolist

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -6,7 +6,11 @@ ToDo List
 
 @section('content')
     {!! view('pages.SectionOpen',['id'=>'view','class'=>NULL]) !!}
-    
+    <ul><li>{!! view('pages.TodoForm') !!}</li>
+        @foreach ($todos as $todo)
+        <li>{{$todo}}</li>
+        @endforeach
+    </ul>
     
     
     {!! view('pages.SectionClose') !!}
