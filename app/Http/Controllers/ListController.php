d32716da6d7c2c26657a451a567236d3aea077af
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-04 16:25:14 -0400

    adding a posthome function for when posting something to the index.  (consider switching controller to a resource controller so the routing is simpeler.)

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -19,5 +19,12 @@ class ListController extends Controller
     public function about() {
         return view('welcome');
     }
+    
+    public function posthome() {
+        // a function to add a task goes here
+        $todos = laraveltodos::all();
+        return view('pages.list', compact('todos'));
+    }
+    
  }
 
