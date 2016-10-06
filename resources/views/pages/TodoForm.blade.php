255d0f26b26bdb4bd3ab22fc2f9e598880277cbd
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-06 17:51:26 -0400

    adding divs of class row    class listText, and listButton to the create form

--- resources/views/pages/TodoForm.blade.php
+++ resources/views/pages/TodoForm.blade.php
@@ -1,7 +1,13 @@
 <form name="AddTask" id="AddTask" novalidate="" method="POST">
-    <input type="hidden" name="_token" value="{{csrf_token()}}">
-    <input id="new-todo" name="new-todo" placeholder="What needs to be done?" autofocus="" class="">
-    <input id="priority" name="priority" type="number" value="5">
-    <button type="submit" class="btn btn-success btn-sm">Add</button>    
+    <div class="row">
+        <div class="listText col-sm-4 todos-item">
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
+            <input id="new-todo" name="new-todo" placeholder="What needs to be done?" autofocus="" class="">
+            <input id="priority" name="priority" type="number" value="5">
+        </div>
+        <div class="listButton col-sm-4 todos-item">
+            <button type="submit" class="btn btn-success btn-sm">Add</button>    
+        </div>
+    </div>
 </form>
 
