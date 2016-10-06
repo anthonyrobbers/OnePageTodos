efd241da18f2a9580ee22b11cfbed8a66728af1c
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-06 11:08:40 -0400

    moving section into list.blade and changing veiw() to @include where appropriate

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -5,7 +5,19 @@ ToDo List
 @stop
 
 @section('content')
+    <section id="todos" class="{{$class}}">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>todos</h2>
+                        <hr class="star-<?php if ($class=='success'){echo'light';}else{echo'primary';}?>">
+                    </div>
+                </div>
 
-    {!! view('pages.Section',['id'=>'todos','class'=>NULL,'content'=>view('pages.tableList', compact('todos'))]) !!}
+                <div class="row">
+                     @include('pages.tableList')
+                </div>
+            </div>
+    </section>
 @stop
     
