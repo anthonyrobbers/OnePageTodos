032f65435fb43d02f9b69c33a33b0d2dce3b0633
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-06 17:54:17 -0400

    adding divs, buttons, and a form to the other list items that are not the create form.  using classes listButton and listText

--- resources/views/pages/tableList.blade.php
+++ resources/views/pages/tableList.blade.php
@@ -1,5 +1,18 @@
-    <ul><li>{!! view('pages/TodoForm') !!}</li>
+
+{!! view('pages/TodoForm') !!}
+        <form name="updateTask" id="updateTask" novalidate="" method="POST" {{ method_field('DELETE') }}
         @foreach ($todos as $todo)
-        <li>{{$todo}}</li>
+        
+            <div class="row">
+                <div class="listText col-sm-4 todos-item">
+                    {{$todo['task']}}
+                </div>
+                <div class="listButton col-sm-4 todos-item">
+                    <button name="del-todo" value="{{$todo['id']}}" type="submit" class="btn btn-success btn-sm">X</button>    
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                </div>
+            </div>
+        
         @endforeach
-    </ul>  
+        </form>
+    
