83cb1b831e175396ee572b74f0364893e50077fa
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-10 10:15:57 -0400

    changing button from form button to a link of class button

--- resources/views/pages/tableList.blade.php
+++ resources/views/pages/tableList.blade.php
@@ -8,8 +8,8 @@
                     {{$todo['task']}}
                 </div>
                 <div class="listButton col-sm-4 todos-item">
-                    <a href="/{{todo['id']}}/edit"class="btn btn-success btn-sm">X</a>
-                    <button name="del-todo" value="{{$todo['id']}}" type="submit" class="btn btn-success btn-sm">X</button>    
+                    <a href="{{action('ListController@edit',['id'=>$todo['id']])}}" class="btn btn-success btn-sm">X</a>
+                    
                     <input type="hidden" name="_token" value="{{csrf_token()}}">
                 </div>
             </div>
