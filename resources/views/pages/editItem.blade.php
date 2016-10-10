5db92eb27086d97d72066d3f8f5cf56a70c19678
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-10 10:14:50 -0400

    switching out all the contents of the div sections to make the form actually delete and edit properly and adding extra rows to label the areas and a second section for a form to delete

--- resources/views/pages/editItem.blade.php
+++ resources/views/pages/editItem.blade.php
@@ -1,20 +1,34 @@
 <div class="listContainer">
-{!! view('pages/TodoForm') !!}
 <form name="updateTask" id="updateTask" novalidate="" method="POST">
-        @foreach ($todos as $todo)
-        
-            <div class="row">
-                <div class="listText col-sm-4 todos-item">
-                    {{$todo['task']}}
-                </div>
-                <div class="listButton col-sm-4 todos-item">
-                    <a href="/{{todo['id']}}/edit"class="btn btn-success btn-sm">X</a>
-                    <button name="del-todo" value="{{$todo['id']}}" type="submit" class="btn btn-success btn-sm">X</button>    
-                    <input type="hidden" name="_token" value="{{csrf_token()}}">
-                </div>
+        <div class="row">
+            <div class="listText col-sm-4 todos-item">
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+                <input id="new-todo" name="new-todo" value="{{$todo['task']}}" autofocus="" class="">
+                <input id="priority" name="priority" type="number" value="{{$todo['priority']}}">
             </div>
+            <div class="listButton col-sm-4 todos-item">
+                <button type="submit" class="btn btn-success btn-sm">Change</button>   
+            </div> 
+        </div> 
+        <div class="row">
+            <div class="listText col-sm-4 todos-item">
+                Old Version:
+            </div>
+            <div class="listButton col-sm-4 todos-item">
+                DELETE
+            </div>
+        </div>
+        <div class="row">
+            <div class="listText col-sm-4 todos-item">
+                {{$todo['task']}}
+            </div>
+            <div class="listButton col-sm-4 todos-item">
+                <button name="del-todo" value="{{$todo['id']}}" type="submit" class="btn btn-success btn-sm">X</button>    
+                <input type="hidden" name="_token" value="{{csrf_token()}}">
+            </div>
+        </div>
         
-        @endforeach
+       
         </form>
 </div>
     
