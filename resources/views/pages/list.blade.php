105fb582bbaae24e82724429fba48c16a95a7038
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-14 14:30:51 -0400

    adding a status region to the list page so it can show a status msg with changes made to the list

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -5,6 +5,52 @@ ToDo List
 @stop
 
 @section('content')
+    @if ($msg)
+        <section id="todos" class="success">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>Status</h2>
+                        <hr class="star-light">
+                    </div>
+                </div>
+
+                <div class="listContainer">
+                    <div class="row">
+                         {{$msg}}
+                    </div>
+                
+                    <div class="row">
+                        <form name="deleteTask" id="updateTask" novalidate="" method="POST" action="{{action('TodoItemController@destroy',['id'=>$currentTodo['id']])}}"> {{ method_field('DELETE') }}
+                        <div class="listText col-sm-4 todos-item">
+                            {{$currentTodo['task']}}  Priority: {{$currentTodo['priority']}}
+                        </div>
+                        <div class="listButton col-sm-4 todos-item">
+                            <button name="del-todo" value="{{$currentTodo['id']}}" type="submit" class="btn btn-success btn-sm">Delete</button>    
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                        </div>
+                        </form>
+                    </div>
+                    <div class="row">
+                         The old version was:
+                    </div>
+                    <div class="row">
+                        <form name="deleteTask" id="updateTask" novalidate="" method="POST" action="{{action('TodoItemController@update',['id'=>$oldtodo['id']])}}"> {{ method_field('PATCH') }}
+                        <div class="listText col-sm-4 todos-item">
+                            {{$oldtodo['task']}} Priority: {{$oldtodo['priority']}}
+                        </div>
+                        <div class="listButton col-sm-4 todos-item">
+                            <button name="del-todo" value="{{$oldtodo['id']}}" type="submit" class="btn btn-success btn-sm">Restore</button>    
+                            <input type="hidden" name="new-todo" value="{{$oldtodo['task']}}" >
+                            <input type="hidden" name="priority" value="{{$oldtodo['priority']}}">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}">
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    @endif
     <section id="todos" class="{{$class}}">
             <div class="container">
                 <div class="row">
