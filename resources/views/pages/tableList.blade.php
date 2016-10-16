a4fcd96f2f708ff4c92622221a13c0f3c921ee5d
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-16 01:56:43 -0400

    simplifying to use todo display sections

--- resources/views/pages/tableList.blade.php
+++ resources/views/pages/tableList.blade.php
@@ -1,20 +1,9 @@
 <div class="listContainer">
-{!! view('pages/TodoForm') !!}
-<form name="updateTask" id="updateTask" novalidate="" method="POST">
-        @foreach ($todos as $todo)
+    {!! view('pages/TodoForm') !!}
+
+    @foreach ($todos as $todo)
+        @include('pages.defaultTodoDisplay')
+    @endforeach
         
-            <div class="row">
-                <div class="listText col-sm-4 todos-item">
-                    <a href="{{action('TodoItemController@edit',['id'=>$todo['id']])}}">{{$todo['task']}}</a>
-                </div>
-                <div class="listButton col-sm-4 todos-item">
-                    <a href="{{action('TodoItemController@edit',['id'=>$todo['id']])}}" class="btn btn-success btn-sm">X</a>
-                    
-                    <input type="hidden" name="_token" value="{{csrf_token()}}">
-                </div>
-            </div>
-        
-        @endforeach
-        </form>
 </div>
     
