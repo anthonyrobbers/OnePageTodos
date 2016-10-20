41d27a6bc9da23e49a722e49518c8b65cde4fa9d
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-20 18:27:50 -0400

    log now says todelete instead of edit in the todelet function and now the edit page there shows using todos class instead of success class

--- app/Http/Controllers/TodoItemController.php
+++ app/Http/Controllers/TodoItemController.php
@@ -279,13 +279,13 @@ class TodoItemController extends Controller
     public function toDelete($id){
         
         // GET  /TodoItem/{id}/delete
-        Log::info('Hit edit ('.$id.') function of the TodoItem controller');
+        Log::info('Hit toDelete ('.$id.') function of the TodoItem controller');
         
         $options=optionList::find(1);
         $todos = TodoItem::find($id);
         if($todos['group']===$options['group']){
             Log::debug('active group detected');
-            return view('pages.edit', ['todo'=>$todos, 'class'=>'success']);
+            return view('pages.edit', ['todo'=>$todos, 'class'=>'todos']);
         }
         else {
             Log::debug('active group not detected');
