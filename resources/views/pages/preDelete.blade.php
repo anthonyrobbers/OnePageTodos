7424e0f336f0f23a7f242861847b47032bca7ac4
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-20 23:04:33 -0400

    adding preDelete page for the toDelete to point to

---
+++ resources/views/pages/preDelete.blade.php
@@ -0,0 +1,24 @@
+@extends('layout')
+
+@section('Title')
+ToDo List - Delete {{$todo['id']}}?
+@stop
+
+@section('content')
+    @include('partials/status')
+    <section id="todos" class="{{$class}}">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2>todo task delete confirmation</h2>
+                        <hr class="star-<?php if ($class=='success'){echo'light';}else{echo'primary';}?>">
+                    </div>
+                </div>
+
+                <div class="row">
+                     @include('pages.editItem')
+                </div>
+            </div>
+    </section>
+@stop
+    
