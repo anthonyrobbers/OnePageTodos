856334833a8be9477057b889322fd8eac7f75b8d
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-28 14:56:45 -0400

    adding an id to the clear complet button as clear-completed to act as a handel for javascript

--- resources/views/partials/clearCompleteButton.blade.php
+++ resources/views/partials/clearCompleteButton.blade.php
@@ -1,11 +1,11 @@
 @if($options['fast'])
-    <form name="ClearComplete" novalidate="" method="POST" action="{{action('TodoItemController@removeAllCompleted',['group'=>$options['group']])}}">
+    <form  name="ClearComplete" novalidate="" method="POST" action="{{action('TodoItemController@removeAllCompleted',['group'=>$options['group']])}}">
         {{ method_field('DELETE') }}
-        <button name="ClearComplete" type="submit" class="btn btn-success btn-sm">Clear Completed</button>
+        <button id="clear-completed" name="ClearComplete" type="submit" class="btn btn-success btn-sm">Clear Completed</button>
         <input type="hidden" name="_token" value="{{csrf_token()}}">
     </form>
 @else
-    <a  href="{{action('TodoItemController@toRemoveAllCompleted',['group'=>$options['group']])}}" class="btn btn-success btn-sm">Clear Completed</a>
+    <a  id="clear-completed" href="{{action('TodoItemController@toRemoveAllCompleted',['group'=>$options['group']])}}" class="btn btn-success btn-sm">Clear Completed</a>
 @endif
   
   
