178d38dd491fd5fab0b06b170a8574c7ba584969
AnthonyRobbers <anthony.robbers@gmail.com>
2016-10-28 14:55:30 -0400

    adding an id to the complete all button to act as a handel for javascript

--- resources/views/partials/batchButtons.blade.php
+++ resources/views/partials/batchButtons.blade.php
@@ -1,5 +1,5 @@
 <div class="btn-group" >
-    <a href="{{action('TodoItemController@markAllComplete')}}" class="btn btn-success btn-sm">Complete All</a>
+    <a id="complete-all" href="{{action('TodoItemController@markAllComplete')}}" class="btn btn-success btn-sm">Complete All</a>
     @include('partials/clearCompleteButton')
 </div>
   
