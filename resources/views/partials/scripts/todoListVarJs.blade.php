87de062dd68763f4c513b8439ea9fba5fc953c6f
AnthonyRobbers <anthony.robbers@gmail.com>
2016-11-01 13:58:48 -0400

    making sure that all numeric types in the initTodos are not counted as strings and are therefore easier to use.

--- resources/views/partials/scripts/todoListVarJs.blade.php
+++ resources/views/partials/scripts/todoListVarJs.blade.php
@@ -1,16 +1,13 @@
 <script id="todo-list-init" >
-    var initTodos = [
-        <?php
+    var initTodos = [<?php
         $translatedList=array();
             foreach($todos as $todo){
-                $translatedList[] = '{"id":"'.$todo['id'].'","task":"'.$todo['task']
-                        .'","priority":"'.$todo['priority'].'","group":"'.$todo['group']
+                $translatedList[] = '{"id":'.$todo['id'].',"task":"'.$todo['task']
+                        .'","priority":'.$todo['priority'].',"group":"'.$todo['group']
                         .'","complete":'.$todo['complete'].'}';  
-                // should look like{"id":"2","title":"test","completed":false},
+                // should look like{"id":2,"task":"test","priority":1,"group":"testing","complete":0},
                 // ['task', 'priority', 'group', 'complete']
             }
             echo implode(',',$translatedList);
-        ?>
-    
-    ];
+        ?>];
 </script>
