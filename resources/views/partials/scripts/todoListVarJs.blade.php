da583ac437c84b554bb0b4ca843388e5d00493d5
AnthonyRobbers <anthony.robbers@gmail.com>
2016-11-01 00:28:35 -0400

    removing "" from the todlist variable blade so it is officially a int instead of a string so it can return false in handelbars

--- resources/views/partials/scripts/todoListVarJs.blade.php
+++ resources/views/partials/scripts/todoListVarJs.blade.php
@@ -5,7 +5,7 @@
             foreach($todos as $todo){
                 $translatedList[] = '{"id":"'.$todo['id'].'","task":"'.$todo['task']
                         .'","priority":"'.$todo['priority'].'","group":"'.$todo['group']
-                        .'","complete":"'.$todo['complete'].'"}';  
+                        .'","complete":'.$todo['complete'].'}';  
                 // should look like{"id":"2","title":"test","completed":false},
                 // ['task', 'priority', 'group', 'complete']
             }
