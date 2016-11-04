2f6ca03deed884fe57dcc20a0c529f9cc775c6a8
AnthonyRobbers <anthony.robbers@gmail.com>
2016-11-03 21:15:04 -0400

    adding a status template script

---
+++ resources/views/partials/scripts/statusTemplate.blade.php
@@ -0,0 +1,18 @@
+<script id="status-template" type="text/x-handlebars-template">
+    
+        
+    <section id="status" class="success">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Status</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+
+            <div class="listContainer">
+                @{{statusBody}}
+            </div>
+        </div>
+    </section>
+</script>
