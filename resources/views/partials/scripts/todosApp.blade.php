d725594db82a2777578196d8831dc3a75f36078a
AnthonyRobbers <anthony.robbers@gmail.com>
2016-11-07 03:53:31 -0500

    adding the status template to the app scripts blade

--- resources/views/partials/scripts/todosApp.blade.php
+++ resources/views/partials/scripts/todosApp.blade.php
@@ -3,6 +3,7 @@
 
 @include('partials/scripts/todoTemplate', ['verbose'=>FALSE])
 @include('handlebarsTemplates/footer')
+@include('partials/scripts/statusTemplate')
 
 <script src="{{asset('node_modules/todomvc-common/base.js')}}"></script>
 <script src="{{asset('node_modules/handlebars/dist/handlebars.js')}}"></script>
