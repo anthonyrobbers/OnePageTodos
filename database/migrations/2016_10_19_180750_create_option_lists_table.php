256add019d8aa2980e7001231f623b067a94303a
AnthonyRobbers <anthony.robbers@gmail.com>
2016-12-05 14:48:44 -0500

    adding defaults to option lists and removing the first attempt at initializing option list 1

--- database/migrations/2016_10_19_180750_create_option_lists_table.php
+++ database/migrations/2016_10_19_180750_create_option_lists_table.php
@@ -16,18 +16,15 @@ class CreateOptionListsTable extends Migration
         Schema::create('option_lists', function (Blueprint $table) {
             $table->increments('id')->comment('1 is the default option set. others can be used for users or saving extra sets for later.');
             
-            $table->string('group',90)->comment('the group to be used');
-            $table->string('style',90)->comment('style sheet type to use default or mvc to start with');
-            $table->integer('filter')->comment('matching complete is displayed 3 displays all');
-            $table->boolean('verbosity')->comment('true or false this displays status');
+            $table->string('group',90)->comment('the group to be used')->default('INDEX');
+            $table->string('style',90)->comment('style sheet type to use default or mvc to start with')->default('todos');
+            $table->integer('filter')->comment('matching complete is displayed 3 displays all')->default(2);
+            $table->boolean('verbosity')->comment('true or false this displays status')->default(TRUE);
             
             $table->timestamps();
         });
         
-        $newOptionList = new optionList(['group'=>'INDEX', 'filter'=>2, 
-                'style'=>'default', 'verbosity'=>true]);
-            // TODO: add first optoin list with the default options to the repository of option lists  
-            $newOptionList->Save();
+        
     }
 
     /**
